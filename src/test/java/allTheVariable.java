f73baa909dcd7d14362db809a7f7788cb5e25b4e
Vasily Borovkov <redison00x@mail.ru>
2024-05-07 18:27:55 +0300

    commfinal1

---
+++ src/test/java/allTheVariable.java
@@ -0,0 +1,55 @@
+public class allTheVariable {
+    public final static String osago = "https://www.rgs.ru/auto/osago";
+    public final static String iSee = "Понятно";
+    public final static String title = "ОСАГО онлайн: калькулятор еОСАГО";
+    public final static String wrong = "Не туда приперся";
+    public final static String clicable = "//a[@class='action-item btn--basic']";
+    public final static String frame = "//*[@id='RESOLUTE_INSURANCE']";
+    public final static String noLicensePlate = "Нет номера";
+    public final static String enginePower1 = "(//div[@class='input-group'])[1]";
+    public final static String enginePower2 = "//*[@placeholder='Мощность двигателя']";
+    public final static String hp = "л.с.";
+    public final static String hP = "//div[text()='л.с.']";
+    public final static String categoryB = "//div[contains(text(),'B')]";
+    public final static String categoryb = "//div[text()='B']";
+    public final static String passanger = "//div[contains(text(),'Легковой')]";
+    public final static String passanger2 = "//div[text()='Легковой']";
+    public final static String personal = "//div[contains(text(),'Личная')]";
+    public final static String personal2 ="//div[text()='Личная']";
+    public final static String stamp ="//div[contains(text(),'Марка')]";
+    public final static String release ="//*[@placeholder='Год']";
+    public final static String model ="//div[contains(text(),'Модель')]";
+    public final static String noVIN ="//*[@for='content.object.data.noVin_custom_input']";
+    public final static String notRegistered = "//*[@for='content.object.data.noGosNumber_custom_input']";
+    public final static String chassis = "//div[contains(text(),'Шасси')]";
+    public final static String chassis1 = "//div[text()='Шасси']";
+    public final static String enterTheNumber = "//*[@placeholder='Укажите номер']";
+    public final static String seriaPTS = "//*[@name='content.object.data.pts.seria']";
+    public final static String numberPTS = "//*[@placeholder='Номер']";
+    public final static String dataPTS = "//*[@placeholder='дд.мм.гггг']";
+    public final static String term1yearC = "//div[contains(text(),'1 год')]";
+    public final static String term1year = "//div[text()='1 год']";
+    public final static String dataOsago = "//*[@name='content.object.data.periods[0].beginDate']";
+    public final static String scroll1Year = "cрок страхования: 1 год";
+    public final static String buttonNext1 = "//button[@class='btn btn-secondary']";
+    public final static String surname = "//*[@name='content.insuredPerson.list[0].lastName']";
+    public final static String limited = "Ограниченное";
+    public final static String name = "//*[@name='content.insuredPerson.list[0].firstName']";
+    public final static String middleName = "//*[@name='content.insuredPerson.list[0].middleName']";
+    public final static String dateOfBirth = "//*[@name='content.insuredPerson.list[0].dob']";
+    public final static String driverLincSeria = "//*[@name='content.insuredPerson.list[0].data.driverLicense.seria']";
+    public final static String driverLincNumber ="//*[@name='content.insuredPerson.list[0].data.driverLicense.number']";
+    public final static String driverLicData = "//*[@name='content.insuredPerson.list[0].data.experienceFrom']";
+    public final static String buttonNo = "//button[@class='btn btn-outline-primary'][2]";
+    public final static String buttonNext2 = "//*[@class='btn btn-secondary']";
+    public final static String login = "//*[@placeholder='Логин (email)']";
+    public final static String password = "//*[@placeholder='Пароль']";
+    public final static String checkBox1 = "Даю ";
+    public final static String buttonNext3 = "//*[@class='my-3 btn btn-secondary btn-block']";
+    public final static String registAddress = "//*[@placeholder='Адрес регистрации']";
+    public final static String buttonNext4 = "//*[@class='btn btn-secondary btn-block']";
+    public final static String checkBox2 ="Страхователь подтверждает, ";
+    public final static String buttonNext5 ="//*[@class='btn btn-secondary btn-block']";
+
+
+}
--- src/test/java/kkk.java
+++ src/test/java/kkk.java
@@ -1,19 +1,12 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.SelenideElement;
-import com.codeborne.selenide.commands.ScrollIntoView;
-import com.codeborne.selenide.commands.ScrollTo;
 import com.codeborne.selenide.selector.ByTextCaseInsensitive;
 import com.codeborne.selenide.selector.WithTextCaseInsensitive;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
-
-import java.awt.*;
 
 import static com.codeborne.selenide.Selenide.*;
 import static org.testng.Assert.assertEquals;
@@ -24,152 +17,148 @@ public class kkk {
     public void osago() {
         Configuration.pageLoadTimeout = 5000;
         Configuration.browserSize = "1920x1080";
-        open("https://www.rgs.ru/auto/osago");
-        
-        sleep(500);
-        $(new ByTextCaseInsensitive("Понятно")).click();
-        sleep(500);
-
-        assertEquals (title(), "ОСАГО онлайн: калькулятор еОСАГО", "Не тудой приперся");
-        Wait().until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@class='action-item btn--basic']"))).click();
-
-        switchTo().frame($x("//*[@id='RESOLUTE_INSURANCE']"));
-        $(new ByTextCaseInsensitive("Нет госномера")).click();
-
-        $x("//div[contains(text(),'Легковые ТС')]").click();
-        $x("//div[text()='Легковые ТС (B)']").click();
-        sleep(500);
-
-        $x("(//div[@class='input-group'])[1]").click();
-        sleep(500);
-        $x("//*[@placeholder='Мощность двигателя']").setValue("87").shouldBe(Condition.visible).pressEnter();
-
-        $(new ByTextCaseInsensitive("л.с.")).click();
-        $x("//div[text()='л.с.']").click();
+        open(allTheVariable.osago);
 
         sleep(500);
-        $x("//div[contains(text(),'Легковой')]").click();
-        $x("//div[text()='Легковой']").click();
-
-        sleep(500);
-        $x("//div[contains(text(),'Личная')]").click();
-        $x("//div[text()='Личная']").click();
-
-        sleep(500);
-        $x("//div[contains(text(),'Марка')]").click();
-        actions().moveToElement($x("//div[contains(text(),'Марка')]")).sendKeys("ВАЗ").pause(500).sendKeys(Keys.chord(Keys.ENTER)).build().perform();
-
-        sleep(500);
-        $x("//*[@placeholder='Год выпуска']").click();
-        actions().moveToElement($x("//*[@placeholder='Год выпуска']")).sendKeys("2012").pause(500).build().perform();
-
-        $x("//div[contains(text(),'Модель')]").click();
-        sleep(500);
-        actions().moveToElement($x("//div[contains(text(),'Модель')]")).sendKeys("2190 Granta").pause(500).sendKeys(Keys.chord(Keys.ENTER)).pause(500).build().perform();
+        $(new ByTextCaseInsensitive(allTheVariable.iSee)).click();
         sleep(500);
 
-        actions().moveToElement($x("//*[@for='content.object.data.noVin_custom_input']")).click();
-        $x("//*[@for='content.object.data.noVin_custom_input']").click(); //нет вин номера
+        assertEquals (title(), allTheVariable.title, allTheVariable.wrong);
+        Wait().until(ExpectedConditions.elementToBeClickable(By.xpath(allTheVariable.clicable))).click();
 
-        actions().moveToElement($x("//*[@for='content.object.data.noGosNumber_custom_input']")).click();
-        $x("//*[@for='content.object.data.noGosNumber_custom_input']").click(); //не поставлен на учёт
+        switchTo().frame($x(allTheVariable.frame));
+        $(new ByTextCaseInsensitive(allTheVariable.noLicensePlate)).click();
 
-        sleep(500);
-        $x("//div[contains(text(),'Шасси')]").scrollIntoView(true).click();
-        $x("//div[text()='Шасси']").click();
+        actions().moveToElement($x(allTheVariable.notRegistered)).click();
+        $x(allTheVariable.notRegistered).click(); //не поставлен на учёт
 
-        sleep(500);
-        $x("//*[@placeholder='Укажите номер']").click();
-        actions().moveToElement($x("//*[@placeholder='Укажите номер']")).sendKeys("X7MXKN7FP6M004128").pause(500).build().perform();
-
-        $(new WithTextCaseInsensitive("Страховое возмещение вреда,")).scrollIntoView(false);
+        actions().moveToElement($x(allTheVariable.noVIN)).click();
+        $x(allTheVariable.noVIN).click(); //нет вин номера
 
         sleep(500);
-        $x("//*[@name='content.object.data.pts.seria']").scrollIntoView(true).click();
-        actions().moveToElement($x("//*[@name='content.object.data.pts.seria']")).sendKeys("62НН").pause(500).build().perform();
+        $x(allTheVariable.chassis).scrollIntoView(true).click();
+        $x(allTheVariable.chassis1).click();
 
         sleep(500);
-        $x("//*[@placeholder='Номер']").click();
-        actions().moveToElement($x("//*[@placeholder='Номер']")).sendKeys("236123").pause(500).build().perform();
+        $x(allTheVariable.enterTheNumber).click();
+        actions().moveToElement($x(allTheVariable.enterTheNumber)).sendKeys("X7MXKN7FP6M004128").pause(500).build().perform();
 
         sleep(500);
-        $x("//*[@placeholder='дд.мм.гггг']").click();
-        actions().moveToElement($x("//*[@placeholder='дд.мм.гггг']")).sendKeys("22.01.2019").pause(500).build().perform();
+        $x(allTheVariable.passanger).click();
+        $x(allTheVariable.passanger2).click();
+
+        $x(allTheVariable.enginePower1).click();
+        sleep(500);
+        $x(allTheVariable.enginePower2).setValue("87").shouldBe(Condition.visible).pressEnter();
+
+        $(new ByTextCaseInsensitive(allTheVariable.hp)).click();
+        $x(allTheVariable.hP).click();
+
+        $x(allTheVariable.categoryB).click();
+        $x(allTheVariable.categoryb).click();
 
         sleep(500);
-        $x("//div[contains(text(),'1 год')]").click();
-        $x("//div[text()='1 год']").click();
+        $x(allTheVariable.personal).click();
+        $x(allTheVariable.personal2).click();
 
         sleep(500);
-        $x("//*[@name='content.object.data.periods[0].beginDate']").click();
-        actions().moveToElement($x("//*[@name='content.object.data.periods[0].beginDate']")).sendKeys("10.05.2024").pause(500).sendKeys(Keys.chord(Keys.ENTER)).build().perform();
-
-        $x("//button[@class='btn btn-secondary']").click();
-
-        $(new WithTextCaseInsensitive("Ограниченное")).click();
+        $x(allTheVariable.stamp).click();
+        actions().moveToElement($x(allTheVariable.stamp)).sendKeys("ВАЗ").pause(500).sendKeys(Keys.chord(Keys.ENTER)).build().perform();
 
         sleep(500);
-        $x("//*[@name='content.insuredPerson.list[0].lastName']").click();
-        actions().moveToElement($x("//*[@name='content.insuredPerson.list[0].lastName']")).sendKeys("Жух").pause(500).build().perform();
+        $x(allTheVariable.release).click();
+        actions().moveToElement($x(allTheVariable.release)).sendKeys("2012").pause(500).build().perform();
 
+        $x(allTheVariable.model).click();
         sleep(500);
-        $x("//*[@name='content.insuredPerson.list[0].firstName']").click();
-        actions().moveToElement($x("//*[@name='content.insuredPerson.list[0].firstName']")).sendKeys("Мух").pause(500).build().perform();
-
-        sleep(500);
-        $x("//*[@name='content.insuredPerson.list[0].middleName']").click();
-        actions().moveToElement($x("//*[@name='content.insuredPerson.list[0].middleName']")).sendKeys("Вах").pause(500).build().perform();
-
-        sleep(500);
-        $x("//*[@name='content.insuredPerson.list[0].dob']").click();
-        actions().moveToElement($x("//*[@name='content.insuredPerson.list[0].dob']")).sendKeys("15.04.1996").pause(500).sendKeys(Keys.chord(Keys.ENTER)).build().perform();
-
-        sleep(500);
-        $x("//*[@name='content.insuredPerson.list[0].data.driverLicense.seria']").click();
-        actions().moveToElement($x("//*[@name='content.insuredPerson.list[0].data.driverLicense.seria']")).sendKeys("9936").pause(500).build().perform();
-
-        sleep(500);
-        $x("//*[@name='content.insuredPerson.list[0].data.driverLicense.number']").click();
-        actions().moveToElement($x("//*[@name='content.insuredPerson.list[0].data.driverLicense.number']")).sendKeys("965481").pause(500).build().perform();
-
-        sleep(500);
-        $x("//*[@name='content.insuredPerson.list[0].data.driverLicense.seria']").click();
-        actions().moveToElement($x("//*[@name='content.insuredPerson.list[0].data.driverLicense.seria']")).sendKeys("9936").pause(500).build().perform();
-
-        sleep(500);
-        $x("//*[@name='content.insuredPerson.list[0].data.experienceFrom']").click();
-        actions().moveToElement($x("//*[@name='content.insuredPerson.list[0].data.experienceFrom']")).sendKeys("10.11.2023").pause(500).sendKeys(Keys.chord(Keys.ENTER)).build().perform();
-
-        $x("//button[@class='btn btn-outline-primary'][2]").click();
-
-        $x("//*[@class='btn btn-secondary']").click();
-
-        sleep(500);
-        $x("//*[@placeholder='Логин (email)']").click();
-        actions().moveToElement($x("//*[@placeholder='Логин (email)']")).sendKeys("redison00x@yandex.ru").pause(500).sendKeys(Keys.chord(Keys.ENTER)).build().perform();
-
-        sleep(500);
-        $x("//*[@placeholder='Пароль']").click();
-        actions().moveToElement($x("//*[@placeholder='Пароль']")).sendKeys("TV23MD").pause(500).sendKeys(Keys.chord(Keys.ENTER)).build().perform();
-
-        $(new WithTextCaseInsensitive("Достоверное ")).click();
-
-        sleep(500);
-        $x("//*[@class='my-3 btn btn-secondary btn-block']").click();
-
-        $x("//*[@placeholder='Адрес регистрации']").click();
-        sleep(500);
-        actions().moveToElement($x("//*[@placeholder='Адрес регистрации']")).sendKeys("г Нижний Новгород, ул Ульянова, д 29, кв. 23").pause(500).sendKeys(Keys.DOWN, Keys.ENTER).pause(500).build().perform();
+        actions().moveToElement($x(allTheVariable.model)).sendKeys("2190 Granta").pause(500).sendKeys(Keys.chord(Keys.ENTER)).pause(500).build().perform();
         sleep(500);
 
-        $x("//*[@class='btn btn-secondary btn-block']").click();
+        sleep(500);
+        $x(allTheVariable.seriaPTS).scrollIntoView(true).click();
+        actions().moveToElement($x(allTheVariable.seriaPTS)).sendKeys("62НН").pause(500).build().perform();
 
         sleep(500);
-        $(new WithTextCaseInsensitive("Страхователь подтверждает, ")).click();
+        $x(allTheVariable.numberPTS).click();
+        actions().moveToElement($x(allTheVariable.numberPTS)).sendKeys("236123").pause(500).build().perform();
 
-        $x("//*[@class='btn btn-secondary btn-block']").click();
+        sleep(500);
+        $x(allTheVariable.dataPTS).click();
+        actions().moveToElement($x(allTheVariable.dataPTS)).sendKeys("22.01.2019").pause(500).build().perform();
+
+        sleep(500);
+        $x(allTheVariable.term1yearC).click();
+        $x(allTheVariable.term1year).click();
+
+        $(new WithTextCaseInsensitive(allTheVariable.scroll1Year)).scrollIntoView(true);
+
+        sleep(500);
+        $x(allTheVariable.dataOsago).click();
+        actions().moveToElement($x(allTheVariable.dataOsago)).sendKeys("15.05.2024").pause(500).sendKeys(Keys.chord(Keys.ENTER)).build().perform();
+
+        $x(allTheVariable.buttonNext1).click();
+
+        $(new WithTextCaseInsensitive(allTheVariable.limited)).click();
+
+        sleep(500);
+        $x(allTheVariable.surname).click();
+        actions().moveToElement($x(allTheVariable.surname)).sendKeys("Жух").pause(500).build().perform();
+
+        sleep(500);
+        $x(allTheVariable.name).click();
+        actions().moveToElement($x(allTheVariable.name)).sendKeys("Мух").pause(500).build().perform();
+
+        sleep(500);
+        $x(allTheVariable.middleName).click();
+        actions().moveToElement($x(allTheVariable.middleName)).sendKeys("Вах").pause(500).build().perform();
+
+        sleep(500);
+        $x(allTheVariable.dateOfBirth).click();
+        actions().moveToElement($x(allTheVariable.dateOfBirth)).sendKeys("15.04.1996").pause(500).sendKeys(Keys.chord(Keys.ENTER)).build().perform();
+
+        sleep(500);
+        $x(allTheVariable.driverLincSeria).click();
+        actions().moveToElement($x(allTheVariable.driverLincSeria)).sendKeys("9936").pause(500).build().perform();
+
+        sleep(500);
+        $x(allTheVariable.driverLincNumber).click();
+        actions().moveToElement($x(allTheVariable.driverLincNumber)).sendKeys("965481").pause(500).build().perform();
+
+        sleep(500);
+        $x(allTheVariable.driverLicData).click();
+        actions().moveToElement($x(allTheVariable.driverLicData)).sendKeys("10.11.2023").pause(500).sendKeys(Keys.chord(Keys.ENTER)).build().perform();
+
+        $x(allTheVariable.buttonNo).click(); //нет
+
+        $x(allTheVariable.buttonNext2).click();
+
+        sleep(500);
+        $x(allTheVariable.login).click();
+        actions().moveToElement($x(allTheVariable.login)).sendKeys("redison00x@yandex.ru").pause(500).sendKeys(Keys.chord(Keys.ENTER)).build().perform();
+
+        sleep(500);
+        $x(allTheVariable.password).click();
+        actions().moveToElement($x(allTheVariable.password)).sendKeys("TV23MD").pause(500).sendKeys(Keys.chord(Keys.ENTER)).build().perform();
+
+        $(new WithTextCaseInsensitive(allTheVariable.checkBox1)).click();
+
+        sleep(500);
+        $x(allTheVariable.buttonNext3).click();
+
+        $x(allTheVariable.registAddress).click();
+        sleep(500);
+        actions().moveToElement($x(allTheVariable.registAddress)).sendKeys("г Нижний Новгород, ул Ульянова, д 29, кв. 23").pause(500).sendKeys(Keys.DOWN, Keys.ENTER).pause(500).build().perform();
+        sleep(500);
+
+        $x(allTheVariable.buttonNext4).click();
+
+        sleep(500);
+        $(new WithTextCaseInsensitive(allTheVariable.checkBox2)).click();
+
+        $x(allTheVariable.buttonNext5).click();
 
         sleep(30000);
+
 
         Configuration.holdBrowserOpen = true;
 
