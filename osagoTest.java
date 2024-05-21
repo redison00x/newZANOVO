4afb9014757686427da9d55df4b4d4f947fba934
redison00x <160257945+redison00x@users.noreply.github.com>
2024-05-21 16:28:58 +0300

    Add files via upload

---
+++ osagoTest.java
@@ -0,0 +1,16 @@
+package test.product.ui;
+
+import org.testng.annotations.Test;
+
+import static product.sravni.osago.Calculate.*;
+
+public class osagoTest {
+    private osagoTest() {
+
+    }
+    @Test
+    public static void startTest() {
+
+        startCalculate();
+    }
+}
